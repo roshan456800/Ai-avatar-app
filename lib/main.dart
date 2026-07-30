0ffc6f483bb0667ab1fba89346002f1add991f27
roshan456800 <roshandalai2580@gmail.com>
2026-07-31 00:20:01 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/home_page.dart';
+import 'package:ai_talking_avatar/screens/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
